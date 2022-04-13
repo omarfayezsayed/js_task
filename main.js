d57616886c5422543c4172051950fc4872781104
omarfayezsayed <you@example.com>
2022-04-13 21:57:36 +0200

    added the toggle

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-// call the api
 let main_var = document.querySelector(".main_div");
-fetch("https://elrady.co/api/group/1#v")
-  .then((response) => response.json())
-  .then((data) => {
-    // console.log(data[0]);
-    for (const val of data) {
-      const html = ` <div  class="sub-group" style="background-image: url('https://elrady.co/images/1:9:Default.jpg');">
-     <h4>${val.GroupName}</h4>
- </div>`;
-      main_var.insertAdjacentHTML("beforeend", html);
-    }
-  });
+const put_html = function (val) {
+  const html = ` <div  class="sub-group" style="background-image: url('https://elrady.co/images/1:9:Default.jpg');">
+  <h4>${val.GroupName}</h4>
+</div>`;
+  main_var.insertAdjacentHTML("beforeend", html);
+};
 
-let btn_event = document.querySelector(".controllers");
+// call the api
 
-btn_event.addEventListener("click", function () {
- 
+async function call_api() {
+  const response = await fetch("https://elrady.co/api/group/1#v");
+  const data = await response.json();
+  console.log(data[0].GroupName);
+
+  for (const val of data) {
+    put_html(val);
+  }
+}
+call_api();
+
+const btn_toggle = document.querySelector(".controllers");
+const dark_mood = document.querySelector("#app");
+btn_toggle.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+  dark_mood.classList.toggle("dark");
 });
